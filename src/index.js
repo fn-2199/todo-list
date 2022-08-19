9a29717af02355b95963939ca515092663a151b1
Pat <alfonsopatrisha@gmail.com>
2022-08-19 12:24:48 -0700

    Create asset folders and add logo

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import './style.css';
+import logo from './assets/images/logo.svg'
 
 const header = document.createElement('header');
 const h1 = document.createElement('h1');
 h1.textContent = "Todo List";
 
-const logo = document.createElement('img');
+let logoImg = new Image();
+logoImg.src = logo;
 
 const main = document.createElement('main');
 
-header.appendChild(h1);
+header.append(h1, logoImg);
 document.body.append(header, main);
